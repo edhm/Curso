41a158a6462bd67448bb297b1dca3008032ecfa0
Edgar Huerta M <ed.huerta.m@gmail.com>
2020-11-03 08:57:59 -0500

    3.9

---
+++ Dominio/CursoInstructor.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Dominio
+{
+    public class CursoInstructor
+    {
+        public int CursoId{get;set;}
+        public Curso Curso{get;set;}
+        public int InstructorId{get;set;}
+        public Instructor Instructor{get;set;}
+    }
+}
---
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPI.Controllers
+{
+    //http://local:5000/[controller]
+    //http://local:5000/WeatherForecast
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> GetJava(){
+            string[] nombresJava=new[]{"JAVA OCA","JAVA OCP", "JAVA SOA","JAVA MICROSERVICES","JAVA ARCHITECT"};
+            return nombresJava;
+        }
+    }
+}
